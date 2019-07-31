dbaf613302113fd2d946532e7783a6b6378344a4
Yi Xin Ang <angyixin@gmail.com>
2019-07-31 15:57:05 +0800

    main with no nav

---
+++ views/templates/main.jsx
@@ -0,0 +1,30 @@
+var React = require("react");
+
+class Main extends React.Component {
+  render() {
+    {/*code here*/}
+    return (
+      <html lang="en">
+      <head>
+        <meta charset="utf-8"/>
+
+        <title>HR Portal</title>
+        <meta name="description" content="HR Portal"/>
+        <meta name="author" content="Artylope"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+
+      </head>
+      <body>
+        { this.props.children }
+      </body>
+      </html>
+
+
+
+
+    );
+  }
+}
+
+module.exports = Main;
